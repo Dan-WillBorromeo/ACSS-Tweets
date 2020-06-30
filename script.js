837c7b05f53f1cb9cb745ba418c7bc4741a0291b
Daniel William R. Borromeo <drborromeo@up.edu.ph>
2020-06-30 13:36:03 +0800

    updated script.js (attempt 2)

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-var tweets = []
+var tweets = JSON.parse(data);
+var words = "";
+var item;
+
+for (item in tweets) {
+    words += "<tr><td>" + tweets[item].created_at + "<br>" + tweets[item].id + "<br>" + tweets[item].text + "<br>" + tweets[item].entities + "</td></tr>";
+}
+
+document.getElementById("placeHolder").innerHTML = words;
