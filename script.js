7f9d6c0e1f514d7fe0e02a5bcc9760be6a3bba99
Daniel William R. Borromeo <drborromeo@up.edu.ph>
2020-06-30 13:48:13 +0800

    updated index.html

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ var data = {
         "entities": {}
     }]
 }
+
 var posts = JSON.parse(data);
 var DBparameters = JSON.stringify(posts);
 var words = "";
 var item;
 
+words += "<tbody>";
 for (item in posts) {
     words += "<tr><td>" + posts[item].created_at + "<br>" + posts[item].id + "<br>" + posts[item].text + "<br>" + posts[item].entities + "</td></tr>";
 }
+words += "</tbody>";
 
 document.getElementById("placeHolder").innerHTML = words;
